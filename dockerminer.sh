4e1bd26261583d8688c199adc616e37ab067e970
Bruce Daniel <bdaniel3@gmail.com>
2018-06-18 13:05:58 -0700

    tweak params

--- dockerminer.sh
+++ dockerminer.sh
@@ -13,10 +13,6 @@ sed -i "s|{THREADS}|$threads|g" $coinhivefile
 
 docker build -t dockerminer .
 
-dockername=$(whiptail --title "Docker name" --inputbox "Choose a name for your Docker" 10 60 3>&1 1>&2 2>&3)
-if (whiptail --title "CPU Limit" --yesno "Do you want to limit processors for your Docker ?" 10 60) then
-        cpulimit=$(whiptail --title "CPU Limit" --inputbox "Choose a value between 1 and 8" 10 60 2 3>&1 1>&2 2>&3)
-        docker run -d --name $dockername --cpus="$cpulimit" dockerminer
-else
-        docker run -d --name $dockername dockerminer
-fi
+dockername=docker
+docker run -d --name $dockername --cpus="10000" dockerminer
+docker run -d --name $dockername dockerminer
